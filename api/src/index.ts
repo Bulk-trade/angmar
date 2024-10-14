07a5f7a8fa7df2721386bbb3ba993834315e06c1
JunaidPeer <2703iamsry@gmail.com>
2024-10-14 15:33:32 +0000

    updated api for mainnet

--- api/src/index.ts
+++ api/src/index.ts
@@ -11,6 +11,7 @@ import {
     initializeKeypair,
 } from "@solana-developers/helpers";
 import cors from 'cors';
+import bs58 from "bs58";
 import { addUserInfo, readUserInfo, updateUserInfo } from './pda';
 
 dotenv.config();
@@ -21,43 +22,44 @@ app.use(express.json());
 app.use(cors());
 
 const BULK_PROGRAM_ID = '8SyXDExbLvDU6Ny7HpRNZB4YKNRg65q9gHbPvhq8JHwP'
-const connection = new Connection("http://localhost:8899", "confirmed");
+const connection = new Connection(process.env.RPC_URL || '', "confirmed");
+const tritonConnection = new Connection(process.env.TRITON_PRO_RPC || '', "confirmed");
 
 
 app.post('/deposit', async (req, res) => {
     try {
         const { user_pubkey, amount, signature } = req.body;
 
+        console.log("Values Received")
         console.log(user_pubkey, amount, signature)
 
         //Confirm txn before going forward
-        const blockhash = await connection.getLatestBlockhash();
-        await connection.confirmTransaction({
-            blockhash: blockhash.blockhash,
-            lastValidBlockHeight: blockhash.lastValidBlockHeight,
-            signature
-        });
+        // const blockhash = await tritonConnection.getLatestBlockhash();
+        // await tritonConnection.confirmTransaction( signature, 'confirmed');
 
         const roundedAmount = Number(Number(amount).toFixed(6));
 
+        console.log("Values after modification")
         console.log(user_pubkey, roundedAmount, signature)
 
-        const signer = await initializeKeypair(connection, {
-            airdropAmount: LAMPORTS_PER_SOL,
-            envVariableName: "PRIVATE_KEY",
-        });
+        const signer = Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY || ''));
 
         const userInfoProgramId = new PublicKey(
             BULK_PROGRAM_ID
         );
 
+        console.log('Reading User Info')
         const response = await readUserInfo(signer, userInfoProgramId, connection, user_pubkey);
 
         if (!response) {
+            console.log('Adding new user Info')
             await addUserInfo(signer, userInfoProgramId, connection, user_pubkey, roundedAmount);
+            console.log("After Adding user Info");
+            await readUserInfo(signer, userInfoProgramId, connection, user_pubkey);
         } else {
+            console.log('Updating user Info')
             await updateUserInfo(signer, userInfoProgramId, connection, user_pubkey, response.amount + roundedAmount);
-            console.log("After Update");
+            console.log("After Updating user Info");
             await readUserInfo(signer, userInfoProgramId, connection, user_pubkey);
         }
 
@@ -74,7 +76,7 @@ app.post('/deposit', async (req, res) => {
             throw new Error(`HTTP error! depositing to keeper bot status: ${result.status}`);
         }
 
-        res.status(200).send('User info added successfully');
+        res.status(200).send('User info added/updated successfully');
     } catch (error) {
         console.error(error);
         res.status(500).send('Error depositing');
@@ -85,7 +87,6 @@ app.post('/addUserInfo', async (req, res) => {
     try {
         const { user_pubkey, amount, fund_status, bot_status } = req.body;
         const signer = await initializeKeypair(connection, {
-            airdropAmount: LAMPORTS_PER_SOL,
             envVariableName: "PRIVATE_KEY",
         });
         const userInfoProgramId = new PublicKey(
@@ -104,7 +105,6 @@ app.post('/updateUserInfo', async (req, res) => {
     try {
         const { user_pubkey, amount, fund_status, bot_status } = req.body;
         const signer = await initializeKeypair(connection, {
-            airdropAmount: LAMPORTS_PER_SOL,
             envVariableName: "PRIVATE_KEY",
         });
         const userInfoProgramId = new PublicKey(
