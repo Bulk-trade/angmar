01f0cd66f9e532047020edfcc88029778a53ca60
Sunit Roy <2703iamsry@gmail.com>
2024-10-24 15:16:03 +0530

    Added initialize_user_stats and initialize_user cpi

--- test.sh
+++ test.sh
@@ -4,19 +4,19 @@
 echo "Initializing vault..."
 curl -X POST http://localhost:4001/initVault \
      -H "Content-Type: application/json" \
-     -d '{"vault_id": "sunit01"}'
+     -d '{"vault_id": "bulk_vault"}'
 echo ""
 
 # Deposit into the vault
 echo "Depositing into vault..."
 curl -X POST http://localhost:4001/deposit \
      -H "Content-Type: application/json" \
-     -d '{"vault_id": "sunit01", "user_pubkey": "sunit", "amount": 1}'
+     -d '{"vault_id": "bulk_vault", "user_pubkey": "sunit", "amount": 1.0}'
 echo ""
 
 # Withdraw from the vault
 echo "Withdrawing from vault..."
 curl -X POST http://localhost:4001/withdraw \
      -H "Content-Type: application/json" \
-     -d '{"vault_id": "sunit01", "user_pubkey": "sunit", "amount": 0.5}'
+     -d '{"vault_id": "bulk_vault", "user_pubkey": "sunit", "amount": 1.0}'
 echo ""
--- vault-program/src/instructions/init_vault.rs
+++ vault-program/src/instructions/init_vault.rs
@@ -2,11 +2,17 @@ use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
     msg,
+    native_token::LAMPORTS_PER_SOL,
     program::invoke_signed,
     program_error::ProgramError,
     pubkey::Pubkey,
     system_instruction,
     sysvar::{rent::Rent, Sysvar},
+};
+
+use drift_interface::{
+    initialize_user_invoke_signed, initialize_user_stats_invoke_signed, InitializeUserAccounts,
+    InitializeUserIxArgs, InitializeUserStatsAccounts, ID,
 };
 
 pub fn initialize_vault(
@@ -21,6 +27,12 @@ pub fn initialize_vault(
 
     let initializer = next_account_info(account_info_iter)?;
     let vault_pda_account = next_account_info(account_info_iter)?;
+    let user = next_account_info(account_info_iter)?;
+    let user_stats = next_account_info(account_info_iter)?;
+    let state = next_account_info(account_info_iter)?;
+    let authority = next_account_info(account_info_iter)?;
+    let payer = next_account_info(account_info_iter)?;
+    let user_rent = next_account_info(account_info_iter)?;
     let system_program = next_account_info(account_info_iter)?;
 
     if !initializer.is_signer {
@@ -28,10 +40,8 @@ pub fn initialize_vault(
         return Err(ProgramError::MissingRequiredSignature);
     }
 
-    let (vault_pda, vault_bump_seed) = Pubkey::find_program_address(
-        &[vault_id.as_bytes().as_ref()],
-        program_id,
-    );
+    let (vault_pda, vault_bump_seed) =
+        Pubkey::find_program_address(&[vault_id.as_bytes().as_ref()], program_id);
 
     if vault_pda != *vault_pda_account.key {
         msg!("Invalid seeds for Vault PDA");
@@ -40,12 +50,13 @@ pub fn initialize_vault(
 
     let rent = Rent::get()?;
     let rent_lamports = rent.minimum_balance(0);
+    let required_lamports = rent_lamports + (0.1 * LAMPORTS_PER_SOL as f64) as u64;
 
     invoke_signed(
         &system_instruction::create_account(
             initializer.key,
             vault_pda_account.key,
-            rent_lamports,
+            required_lamports,
             0,
             program_id,
         ),
@@ -54,13 +65,47 @@ pub fn initialize_vault(
             vault_pda_account.clone(),
             system_program.clone(),
         ],
-        &[&[
-            vault_id.as_bytes().as_ref(),
-            &[vault_bump_seed],
-        ]],
+        &[&[vault_id.as_bytes().as_ref(), &[vault_bump_seed]]],
     )?;
 
     msg!("PDA created: {}", vault_pda);
+
+    msg!("drift_interface::ID: {}", ID);
+
+    //Initialize user stats
+    initialize_user_stats_invoke_signed(
+        InitializeUserStatsAccounts {
+            user_stats,
+            state,
+            authority,
+            payer,
+            rent: user_rent,
+            system_program,
+        },
+        &[&[vault_id.as_bytes().as_ref(), &[vault_bump_seed]]],
+    )?;
+
+    let mut name = [0u8; 32];
+    let bytes = vault_id.as_bytes();
+    name[..bytes.len()].copy_from_slice(bytes);
+
+    //Initialize user
+    initialize_user_invoke_signed(
+        InitializeUserAccounts {
+            user,
+            user_stats,
+            state,
+            authority,
+            payer,
+            rent: user_rent,
+            system_program,
+        },
+        InitializeUserIxArgs {
+            sub_account_id: 0,
+            name: name,
+        },
+        &[&[vault_id.as_bytes().as_ref(), &[vault_bump_seed]]],
+    )?;
 
     Ok(())
 }
