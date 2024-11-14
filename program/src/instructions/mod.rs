31d0ff13824b44e2cddab944339e133a8cd3031b
Sunit Roy <2703iamsry@gmail.com>
2024-11-14 17:36:13 +0530

    file name changes

--- program/src/instructions/mod.rs
+++ program/src/instructions/mod.rs
@@ -1,9 +1,9 @@
-pub mod init_vault;
+pub mod initialize_vault;
 pub mod deposit;
 pub mod withdraw;
-pub mod init_drift;
+pub mod initialize_drift;
 
-pub use init_vault::*;
+pub use initialize_vault::*;
 pub use deposit::*;
 pub use withdraw::*;
-pub use init_drift::*;
+pub use initialize_drift::*;
